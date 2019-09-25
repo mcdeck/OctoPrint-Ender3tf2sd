495a51d5f567f8f5c75dcc611e90e993fecb981c
Oliver van Porten <oliver@van-porten.de>
2019-09-25 22:12:03 +0200

    actually add some functionality

--- octoprint_ender3tf2sd/__init__.py
+++ octoprint_ender3tf2sd/__init__.py
@@ -1,39 +1,15 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
-
 import octoprint.plugin
 
-class Ender3tf2sdPlugin(octoprint.plugin.SettingsPlugin,
-                        octoprint.plugin.AssetPlugin,
-                        octoprint.plugin.TemplatePlugin):
+class Ender3tf2sdPlugin(octoprint.plugin.OctoPrintPlugin):
 
-	##~~ SettingsPlugin mixin
-
-	def get_settings_defaults(self):
-		return dict(
-			# put your plugin's default settings here
-		)
-
-	##~~ AssetPlugin mixin
-
-	def get_assets(self):
-		# Define your plugin's asset files to automatically include in the
-		# core UI here.
-		return dict(
-			js=["js/ender3tf2sd.js"],
-			css=["css/ender3tf2sd.css"],
-			less=["less/ender3tf2sd.less"]
-		)
-
-	##~~ Softwareupdate hook
+	def convert_tf_to_sd(self, comm, line, *args, **kwargs):
+		if "TF" not in line:
+			return line
+		line.replace("TF","SD")
+		return line
 
 	def get_update_information(self):
 		# Define the configuration for your plugin to use with the Software Update
@@ -67,6 +43,7 @@ def __plugin_load__():
 
 	global __plugin_hooks__
 	__plugin_hooks__ = {
-		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
+		"octoprint.comm.protocol.gcode.received": __plugin_implementation__.convert_tf_to_sd,
 	}
 
